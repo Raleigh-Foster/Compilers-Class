e1956b817331aa7e486022f9a07ea681640f7b76
Raleigh-Foster <ralsusbrad@aol.com>
2017-03-25 13:38:13 -0400

    work

--- HW4/HW5/build.sh
+++ HW4/HW5/build.sh
@@ -1,11 +1,11 @@
 happy hap.y -o hap.hs
 ghc hap.hs -o hap
-./hap < testfoo.qk > myTerrible.c
+./hap < final1.qk > myTerrible.c
 
 cat start.c myTerrible.c closingBrace > augmentedAwful.c
 
 gcc -g -O -c Builtins.c 2> donotcare
-gcc augmentedAwful.c Builtins.c -o yeaLikeThisIsGoingToRunCorrectly 2> donotcare2
+gcc -w augmentedAwful.c Builtins.c -o yeaLikeThisIsGoingToRunCorrectly 2> donotcare2
 ./yeaLikeThisIsGoingToRunCorrectly
 
 
